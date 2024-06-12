f5a8d1cd76e759a87c842d34a3f0fe3da55f04cf
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2024-06-12 23:34:39 +0900

    Add files via upload

---
+++ JS Hamburgers/script.js
@@ -0,0 +1,10 @@
+let icons = document.querySelectorAll(".icon");
+
+for (let i = 0; i < icons.length; i++) {
+  icons[i].addEventListener("click", (e) => {
+    let target = e.target;
+    target.classList.toggle("active");
+    // console.log(target)
+  })
+
+}
